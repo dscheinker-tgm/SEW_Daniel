b78e82ed5df0c10924f020bfb76801853d00dcb8
Daniel Scheinker <dscheinker@student.tgm.ac.at>
2018-10-20 18:06:57 +0200

    Danke Niko

---
+++ src/Hallo_Daniel2.java
@@ -0,0 +1,12 @@
+
+public class Hallo_Daniel2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("PAZAN");
+		System.out.println("PAZAN");
+		System.out.println("PAZAN");
+
+	}
+
+}
